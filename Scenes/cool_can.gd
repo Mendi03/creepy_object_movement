9c4e6e8194502eaa3f09db8d08d37a604fa8c146
Mendi03 <salva.mendis.03@gmail.com>
2024-04-11 13:41:17 -0500

    figured out raycast

--- Scenes/cool_can.gd
+++ Scenes/cool_can.gd
@@ -4,56 +4,70 @@ const OCC_RAY_TARGET_Y_OFFSET = 0.5
 
 var _occlusion_check_rays : Array[RayCast3D]
 var is_looked_at = true
+var can_moved = false
+var has_collided = false
+var previous_position : Vector3
+var ready_to_move = false
 
-@export var target_player : CharacterBody3D
+@onready var target_player = $"../Player"
 
 @onready var vis = $VisibleOnScreenNotifier3D
 @onready var Occlusion_Check_Rays_Parent = $OcclusionCheckRaysParent
 
 func _ready():
+	
+	if not target_player:
+		printerr(self.name + " has no player target")
+		set_physics_process(false)
+		return
+		
 	for r in Occlusion_Check_Rays_Parent.get_children():
 		if r is RayCast3D:
-			#r.add_exception(target_player) 
+			r.add_exception(self) 
+			r.add_exception(target_player) 
 			_occlusion_check_rays.append(r)
+	
+	#previous_position = global_position
 	
 	
 func _physics_process(_delta):
-	is_looked_at = _is_viewed()
+	var on_screen = vis.is_on_screen()
+	var behind_obstacle = _ray_colliding()
 	
-	if is_looked_at:
+	if on_screen and behind_obstacle:
+		_move_can()
+	elif not on_screen and behind_obstacle:
 		return
-		
-	var teleportTarget = Vector3(0, 1, 6)
-	var teleportTarget2 = Vector3(6.846, 1, -6.406)
-	var teleportTarget3 = Vector3(-7.36, 1, -6.406)
+	elif not behind_obstacle and on_screen:
+		return
+	elif (not on_screen) and (not behind_obstacle):
+		_move_can()
 
-	var positions = [teleportTarget, teleportTarget2, teleportTarget3]
-
-	global_transform.origin = positions.pick_random()
-
-#func _on_visible_on_screen_notifier_3d_2_screen_exited():
-	#global_transform.origin = positions.pick_random()
-
-func _is_viewed() -> bool:
-	var viewed = vis.is_on_screen()
-	
-	# if statue not on screen, we can already stop
-	if not viewed:
-		return viewed
-	
-	'''
+func _ray_colliding() -> bool:
 	var colliding_rays = 0
+	var behind_obst : bool
 	
 	# make raycasts point to player position and count how many are colliding with an obstacle
 	for r in _occlusion_check_rays:
 		r.target_position = (target_player.global_position - r.global_position) * self.basis
 		r.target_position.y += OCC_RAY_TARGET_Y_OFFSET
-		if viewed and r.is_colliding():
+		if r.is_colliding():
 			colliding_rays += 1
-	
-	# if all raycasts are colliding, the statue is hidden by an obstacle
+	# if all raycasts are colliding, the can is hidden by an obstacle
 	if colliding_rays >= _occlusion_check_rays.size():
-		viewed = false
-	'''
-	return viewed
+		behind_obst = true
+		return behind_obst
+	
+	behind_obst = false
+	return behind_obst
+	
+	
+func _move_can():
+	var coord = Vector3(0, 1, 6)
+	var coord2 = Vector3(6.846, 1, -6.406)
+	var coord3 = Vector3(-7.36, 1, -6.406)
+	var positions = [coord, coord2, coord3]
+
+	global_position = positions.pick_random()
+	return
 
